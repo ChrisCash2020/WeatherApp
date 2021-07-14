b7f876d291251bac0c8b178dd98dd7fa01937046
ChrisCash2020 <81109855+ChrisCash2020@users.noreply.github.com>
2021-07-13 23:37:50 -0500

    current temp info done

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import mainStyles from './style.css';
 let AllDates = document.querySelectorAll('.date');
-let datehtml = document.querySelector('.todays-date');
+let dateHtml = document.querySelector('.todays-date');
+let currentTempHtml = document.getElementById('current-temp');
+let feelsLikeHtml = document.getElementById('feels-like');
+let humidHtml = document.getElementById('humid');
+let windHtml = document.getElementById('wind');
+let cityHtml = document.querySelector('.city');
+let weatherDescHtml = document.querySelector('.weather-desc');
 var d = new Date();
 let tracker = d.getDay();
 let todaysDate = d.toDateString();
-datehtml.textContent = `${todaysDate.slice(
+dateHtml.textContent = `${todaysDate.slice(
   0,
   todaysDate.length - 4
 )}${d.toLocaleString('en-US', {
@@ -29,7 +35,10 @@ var weekday = [
   'Saturday',
 ];
 let farenheitt = (temp) => {
-  return (temp - 273.15) * (9 / 5) + 32;
+  return Math.round((temp - 273.15) * (9 / 5) + 32);
+};
+let celcius = (temp) => {
+  return Math.round(temp - 273.15);
 };
 AllDates.forEach((date) => {
   tracker++;
@@ -38,37 +47,56 @@ AllDates.forEach((date) => {
 
 const AllTemps = document.querySelector('.temp');
 
-async function getCoords() {
-  const response = await fetch(
-    'https://api.openweathermap.org/data/2.5/weather?q=celina&APPID=2956f1f443ea71e7c2f75fc5924b8aff',
-    { mode: 'cors' }
-  );
-  const coorData = await response.json();
-  let lat = coorData.coord.lat;
-  let lon = coorData.coord.lon;
-  getStuff(lat, lon);
-}
-getCoords();
-
-// async function getStuff(lat, lon) {
-//   const response = await fetch(
-//     `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly,minutely&appid=2956f1f443ea71e7c2f75fc5924b8aff`,
-//     { mode: 'cors' }
-//   );
-//   const tempData = await response.json();
-//   console.log(tempData);
+// function getLocation() {
+//   if (navigator.geolocation) {
+//     navigator.geolocation.getCurrentPosition(showPosition);
+//   } else {
+//     x.innerHTML = 'Geolocation is not supported by this browser.';
+//   }
 // }
-class Weather {
-  constructor(lat, lon) {
-    (this.lat = lat), (this.lon = lon);
-  }
-  async getData(lat, lon) {
+
+// function showPosition(position) {
+
+//   );
+// }
+
+let startWeatherApp = () => {
+  async function getCoords() {
     const response = await fetch(
-      `https://https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly&appid=2956f1f443ea71e7c2f75fc5924b8aff`,
+      'https://api.openweathermap.org/data/2.5/weather?q=celina&APPID=2956f1f443ea71e7c2f75fc5924b8aff',
+      { mode: 'cors' }
+    );
+    const coorData = await response.json();
+    console.log(coorData);
+    let lat = coorData.coord.lat;
+    let lon = coorData.coord.lon;
+    cityHtml.textContent = `${coorData.name}, ${coorData.sys.country}`;
+    weatherDescHtml.textContent = `${coorData.weather[0].description}`;
+    getStuff(lat, lon);
+  }
+  getCoords();
+
+  async function getStuff(lat, lon) {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly,minutely&appid=2956f1f443ea71e7c2f75fc5924b8aff`,
       { mode: 'cors' }
     );
     const tempData = await response.json();
     console.log(tempData);
+    let fTemp = farenheitt(tempData.current.temp);
+    let cTemp = celcius(tempData.current.temp);
+    let feelFTemp = farenheitt(tempData.current.feels_like);
+    let feelCTemp = celcius(tempData.current.feels_like);
+    let fSign = '°F';
+    let cSignt = '°C';
+    currentTempHtml.style.letterSpacing = '-1px';
+    currentTempHtml.textContent = `${fTemp} ${fSign}`;
+    feelsLikeHtml.textContent = `${feelFTemp} ${fSign}`;
+    humidHtml.textContent = `${tempData.current.humidity}%`;
+    windHtml.textContent = `${tempData.current.wind_speed} mph`;
+    for (var i = 0; i < tempData.daily.length; i += 8) {
+      console.log(tempData.daily[i].dt_txt);
+    }
   }
-}
-let celina = new Weather('Celina');
+};
+startWeatherApp();
